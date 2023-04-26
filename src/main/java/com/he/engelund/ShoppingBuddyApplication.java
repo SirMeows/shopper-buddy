a6089613f2b39164c3d606b347783ac1bcae12d4
SirMeows <araelbrighteyes@gmail.com>
2023-04-26 16:34:35 +0200

    changed db related application.properties

--- src/main/java/com/he/engelund/ShoppingBuddyApplication.java
+++ src/main/java/com/he/engelund/ShoppingBuddyApplication.java
@@ -3,10 +3,12 @@ package com.he.engelund;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.he.engelund")
 public class ShoppingBuddyApplication {
 
     @Bean
