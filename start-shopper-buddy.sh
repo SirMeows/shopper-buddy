e58be4cae58836c2a93596ff2409bab3457ecb47
SirMeows <araelbrighteyes@gmail.com>
2024-04-03 19:56:10 +0200

    remove b64 decoding of secrets

--- start-shopper-buddy.sh
+++ start-shopper-buddy.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 #Check if variable exists
-if [ -z "$GOOGLE_OAUTH2_CLIENT_ID_B64ENC" ]; then
-    echo "GOOGLE_OAUTH2_CLIENT_ID_B64ENC is not set."
+if [ -z "$GOOGLE_OAUTH2_CLIENT_ID" ]; then
+    echo "GOOGLE_OAUTH2_CLIENT_ID is not set."
     exit 1
 fi
-if [ -z "$GOOGLE_OAUTH2_CLIENT_SECRET_B64ENC" ]; then
-    echo "GOOGLE_OAUTH2_CLIENT_SECRET_B64ENC is not set."
+if [ -z "$GOOGLE_OAUTH2_CLIENT_SECRET" ]; then
+    echo "GOOGLE_OAUTH2_CLIENT_SECRET is not set."
     exit 1
 fi
-
-
-#Set values
-export GOOGLE_OAUTH2_CLIENT_ID=$GOOGLE_OAUTH2_CLIENT_ID_B64ENC
-export GOOGLE_OAUTH2_CLIENT_SECRET=$GOOGLE_OAUTH2_CLIENT_SECRET_B64ENC
 
 #Run the application
 java -Djava.security.egd=file:/dev/./urandom -jar /app/shopper-buddy.jar
