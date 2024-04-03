f45baa4e762d2511a119a7ab810d4029fc8d3d34
SirMeows <araelbrighteyes@gmail.com>
2024-04-03 19:44:22 +0200

    dont decode envvars

--- start-shopper-buddy.sh
+++ start-shopper-buddy.sh
@@ -12,8 +12,8 @@ fi
 
 
 #Set values
-export GOOGLE_OAUTH2_CLIENT_ID=$(echo "$GOOGLE_OAUTH2_CLIENT_ID_B64ENC" | base64 --decode)
-export GOOGLE_OAUTH2_CLIENT_SECRET=$(echo "$GOOGLE_OAUTH2_CLIENT_SECRET_B64ENC" | base64 --decode)
+export GOOGLE_OAUTH2_CLIENT_ID=$GOOGLE_OAUTH2_CLIENT_ID_B64ENC
+export GOOGLE_OAUTH2_CLIENT_SECRET=$GOOGLE_OAUTH2_CLIENT_SECRET_B64ENC
 
 #Run the application
 java -Djava.security.egd=file:/dev/./urandom -jar /app/shopper-buddy.jar
